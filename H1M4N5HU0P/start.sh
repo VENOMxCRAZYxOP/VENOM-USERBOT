ea01ef6ab80cf063875fbdb3c2882b8857d6fba0
VENOMxCRAZYxOP <98822762+VENOMxCRAZYxOP@users.noreply.github.com>
2022-02-09 04:00:05 +0530

    Add files via upload

---
+++ H1M4N5HU0P/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#   Copyright (C) 2021 Mafiabot
+#
+#   Poject VΛDΣЯ is free software: 
+#   you can redistribute it and/or modify it under the terms of the GNU General Public License 
+#   as published by the Free Software Foundation, either version 3 of the License, 
+#   or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, 
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with this program. 
+#   If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+echo """
+
+                       🐉😡  l𝔢Ğε𝕟ｄ ᐯẸ𝔫ᗝ𝓜 乃ᗝ𝓣  🎃🍬 !!
+                                          @v3NOM_Support
+"""
+python3 -m userbot
